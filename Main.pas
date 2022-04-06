441e702d4e3b7c5dc494ce07bef9d75007ed61e4
Andrey Sergeev <as111004andr@gmail.com>
2022-04-06 20:06:27 +0700

    Подчистили, запись консоли в файл, замена вайл на фор, мелкие правки

--- Main.pas
+++ Main.pas
@@ -1,45 +1,79 @@
 ﻿program WriteBinaryData;
 
 ///if free pascal / если фри паскаль раскомментить
-//uses windows, sysutils, classes;
+uses windows, sysutils, classes;
 
 const
-  C_FNAME = '..\NoExam\Resources\';
+  IN_PATH = 'C:\Users\as44s\Desktop\Study\Pascal\NoExam\In_Files\';
+  ///console file / файл того что в консоли
+  OUT_PATH = 'C:\Users\as44s\Desktop\Study\Pascal\NoExam\Out_Files\Out1.txt';
 
 var
-  tf: TextFile;
-  s, all: string;
-  i : integer;
+  //text var.
+  tfIn, tfOut, tf: TextFile;
+  //other var.
+  s: string;
+  i: integer;
 
-procedure Ex(p: string);
+procedure ResetFile(s: string);
 begin
-  Assign(tf, C_FNAME+p);
+  Assign(tf, s);
+  Rewrite(tf);
+  Close(tf);
+end;
 
-  writeln('Read: ', p);
+procedure Print(s: string);
+begin
+  Assign(tfOut, OUT_PATH);
+  Append(tfOut);
+  Write(s);
+  Write(tfOut, s);
+  Close(tfOut);
+end;
+
+procedure Println(s: string);
+begin
+  Assign(tfOut, OUT_PATH);
+  Append(tfOut);
+  Writeln(s);
+  Writeln(tfOut, s);
+  Close(tfOut);
+end;
+
+procedure FileToByte(name: string);
+begin
+  Assign(tf, IN_PATH + name);
+  
+  Println('Read: ' + IN_PATH + name);
   reset(tf);
   while not eof(tf) do
   begin
     readln(tf, s);
-    for var i := 1 to Length(s) do
+    i := 1;
+    while (i < Length(s)) do
     begin
+      Inc(i);
+      
       if (s[i] <> '.') then
-        begin
-//          all := all + ord(s[i]), ' ';
-          write(ord(s[i]), ' ')
-        end
-      else writeln();
-      end;
+      begin
+        Print(IntToStr(ord(s[i])) + ' ');
+      end
+      else Println('');
+      
+    end;
   end;
+  
   Close(tf);
-  writeln();
+  Println('');
 end;
 
 begin
-
-  Ex('dataANSI.txt');
-  Ex('dataDOS.txt');
-  Ex('dataUTF8.txt');
-  Ex('dataUTF16LE.txt');
-
+  ResetFile(OUT_PATH);
+  
+  FileToByte('dataANSI.txt');
+  FileToByte('dataDOS.txt');
+  FileToByte('dataUTF8.txt');
+  FileToByte('dataUTF16LE.txt');
+  
   readln;
 end.
